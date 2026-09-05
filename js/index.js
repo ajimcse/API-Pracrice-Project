cb22fa411d5ed6e85fe1c32deeb9697dd1e1160b
ajimcse <mdajimsorkarcse40@gmail.com>
2026-09-05 23:40:59 +0600

    time convert kore

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
+function getTimeString(time) {
+    const hour = parseInt(time / 3600);
+    const minit = parseInt((time % 3600) / 60);
+    const second = parseInt(time % 60);
+
+    return `${hour} Hour ${minit} minit ${second} second ago`;
+}
+
 // button start
 const loadCategroies = () => {
     fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
@@ -33,11 +41,15 @@ const displayVideo = (videos) => {
         const card = document.createElement('div');
         card.innerHTML = `
         <div class="card">
-            <figure class="h-[200px]">
-                <img class="w-full h-full object-cover"
+            <figure class="h-[200px] relative">
+                <img  class="w-full h-full object-cover"
                     src="${video.thumbnail}"
                     alt="Video Thumbnail"
                 />
+                ${
+                    video.others.posted_date?.length ==0?"" :`<span  class=" absolute right-2 bg-black text-white p-1 rounded bottom-2" >${getTimeString(video.others.posted_date)}</span>`
+                }
+                
             </figure>
             <div class="px-0 py-2">
                 <div class="flex gap-3">
@@ -46,10 +58,10 @@ const displayVideo = (videos) => {
                     ${video.title}
                   </h2>
                 
-                 <img class="text-gray-400 w-5 h-5" src="https://img.icons8.com/?size=48&id=SRJUuaAShjVD&format=png" alt="">
+               ${video.authors[0].verified ==true ? `  <img class="text-gray-400 w-5 h-5" src="https://img.icons8.com/?size=48&id=SRJUuaAShjVD&format=png" alt="">`:''}
               </div>
 
-              <div>
+              <div> 
                  <p>
                  ${video.authors[0].profile_name}
                </p>
@@ -64,10 +76,30 @@ const displayVideo = (videos) => {
 
 loadVideos();
 
-// video End
-//    <div class="">
-//
-//                  <img src="https://img.icons8.com/?size=48&id=SRJUuaAShjVD&format=png" alt="">
-//                 </div>
-//                 <div>
-//                 </div>
+// // {
+//   "status": true,
+//   "message": "successfully fetched all the videos",
+//   "videos": [
+//     {
+//       "category_id": "1001",
+//       "video_id": "aaaa",
+//       "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+//       "title": "Shape of You",
+//       "authors": [
+//         {
+//           "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+//           "profile_name": "Olivia Mitchell",
+//           "verified": ""
+//         }
+//       ],
+//       "others": {
+//         "views": "100K",
+//         "posted_date": "16278"
+//       },
+//       "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+//     },//
+
+
+
+
+
